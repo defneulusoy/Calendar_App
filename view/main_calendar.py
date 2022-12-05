426cb44926c6a335a8d559dac769329d0f1484d9
ethanshan1 <eyshan72@gmail.com>
2022-12-05 12:10:31 -0500

    small changes

--- view/main_calendar.py
+++ view/main_calendar.py
@@ -1,21 +1,30 @@
+from controller import action_selector
+from templates import all_months
 
-calendar_type = input("Please enter 'new' to add a new calendar, or the date of the calendar in the 'dd-mm-year' format if you would like to see an existing calendar: ")
+
+
+calendar_type = input("Please enter 'new' to add a new calendar, or the pathway to an old calendar if you would like to see an existing calendar: ")
 
 if calendar_type == 'new':
     start_year = input('Which year would you like your calendar to start from?')
-    with open('New_Calendar.txt', 'w') as f:
-        #initialize new calendar
-        pass
+    final_cal = all_months.months_all(start_year)
 else:
-    date_lst = calendar_type.split('-')
-    day = date_lst[0]
-    month = date_lst[1]
-    year = date_lst[2]
-    print("Welcome to your personalized calendar application")
-    print("Here, you can do many tasks such as adding, deleting or modifying various types of pre_determined or new events, and save all your events to view them later")
-    print("Here are some suggestions, please enter your selection number")
+    pathway = calendar_type
+    x = ... # read file's objects
+
+
+print("Welcome to your personalized calendar application")
+print("Here, you can do many tasks such as adding, deleting or modifying various types of pre_determined or new events, and save all your events to view them later")
+print("Here are some suggestions, please enter your selection number")
+x = "Y"
+while x:
     print("Number 1: Add a new event")
     print("Number 2: Delete an existing event")
     print("Number 3: Modify a pre-existing event")
+    action_selection = int(input(print("Please enter your selection: ")))
+    action_selector.ActionSelection(action_selection)
+    x = input("Whould you like to make more changes? (Y/N)")
 
-    action_selection = input(print("Please enter your selection: "))
+print("The file will now automatically save.")
+
+... # write the calendar into the file
